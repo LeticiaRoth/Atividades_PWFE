34829a679e1d44e41dbea7ad4a1c2c69ab0f6529
LeticiaRoth <leticiaroth1@gmail.com>
2025-08-11 16:03:22 -0300

    Anotações em aula..

--- Aulas/Aula 2 - JavaScript/script.js
+++ Aulas/Aula 2 - JavaScript/script.js
@@ -1,2 +1,76 @@
-nome = prompt("Digite seu nome:")
-console.log("Seja Bem vindo a aula de hoje",nome)
+// Três elementos Contador, Condição de Saída e Iterador
+
+console.log("FOR")
+//Exemplo FOR
+let cats = ["Kuromi", "Kitty", "Purim", "Roll"];
+let info = "My cats are called ";
+
+//Volta ele colocando dentro da info até acabar o tamanho da lista
+for (let i = 0; i < cats.length; i++){
+    info += cats[i] + ", ";
+}
+console.log("EXEMPLO FOR:", info);
+
+//Prático FOR
+let cachorros = ["Lola", "Amora", "Luna", "Bob"];
+let frase = "Eu adoro o ";
+
+for (let i = 0; i < cats.length; i++){
+    frase += cachorros[i] + ", "
+}
+console.log("PRÁTICO FOR:", frase);
+
+//Console de separação de linhas
+console.log("")
+
+console.log("WHILE")
+//Exemplo WHILE
+let gatos = ["Kuromi", "Kitty", "Purim", "Roll"];
+let completo = "My cats are called ";
+let i = 0;
+
+while (i < gatos.length){
+    if (i == gatos.length - 1){
+        completo += "e " + gatos[i] + "."
+    }else{
+        completo += gatos[i] + ", "
+    }
+    i++;
+}
+console.log("EXEMPLO WHILE:", completo);
+
+
+//Console de separação de linhas
+console.log("");
+
+console.log("DO WHILE");
+//Exemplo DO WHILE
+//So válida depois que faz todas as verificações, faça enquanto
+
+let caes = ["Kuromi", "Kitty", "Purim", "Roll"];
+let fraseCompleta = "My cats are called ";
+let indice = 0;
+
+do {
+    if (indice === gatos.length - 1) {
+        fraseCompleta += "e " + gatos[indice] + ".";
+    } else {
+        fraseCompleta += gatos[indice] + ", ";
+    }
+    indice++;
+} while (indice < gatos.length);
+
+console.log(fraseCompleta);
+
+
+//Console de separação de linhas
+console.log("");
+
+//Explicação de Funções
+//Embutidas no navegador, um bloco de código reutilizável que pode ser executado quando chamado;
+//Para usar uma função devemos fazer a declaraçãio dela, com a seguinte sintaxe;
+//function name(){}
+console.log("FUNÇÕES");
+
+//Função anônima
+//
